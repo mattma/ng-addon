be431373d135f3e58cc0bfcadecb1e7a003c5ba8
Matt Ma <bigmabig@gmail.com>
2016-06-22 17:25:56 -0700

    cleanup logging message for create command

--- src/commands/create/index.js
+++ src/commands/create/index.js
@@ -1,9 +1,9 @@
 'use strict';
 
-var Promise = require('bluebird');
-var resolve = require('path').resolve;
-var tildify = require('tildify');
-var gutil   = require('gulp-util');
+var Promise     = require('bluebird');
+var resolvePath = require('path').resolve;
+var tildify     = require('tildify');
+var gutil       = require('gulp-util');
 
 var createRootContent = require('./create-root-content');
 var createRootFiles   = require('./create-root-files');
@@ -21,26 +21,39 @@ var bold    = gutil.colors.bold;
 
 // Step 2: installation step: internet access is required
 // Flow Control: execute serial tasks: npm install, git init
-function installerTasks (addonName, dest) {
-  var tasks = [
+function installerTasks (dest) {
+  var gitTasks = [
     isBinaryExist('git'),
-    npmInstall(dest),
-    gitInitialization(dest)
+    gitInitialization(dest),
   ];
 
-  Promise.all(tasks)
+  var npmTasks = [
+    npmInstall(dest)
+  ];
+
+  // git initialization task
+  Promise.all(gitTasks)
     .then(function () {
-      successInfoLogger(addonName);
+      successLogger('Successfully initialized git.');
     })
-    .catch(function (err) {
-      // output error for individual task
-      failureInfoLogger(err);
-    });
+    .catch(errorLogger);
+
+  // npm installation task
+  log(gray('[-log:]'), 'Installing packages for tooling via npm...');
+
+  Promise.all(npmTasks)
+    .then(function () {
+      successLogger('Installed packages for tooling via npm.');
+      log(bold('[-copy:] =>'),
+        cyan('cd ' + addonName), gray('# navigate to the newly created addon package'));
+      log(bold('[-copy:] =>'), cyan('ngg serve'), gray('watch file changes and rebuild the addon'));
+    })
+    .catch(errorLogger);
 }
 
 // Step 1: run sequentially tasks to scaffold addon packages: copy addon core skeleton
 module.exports = function runTasks (addonName, options) {
-  var dest          = resolve(addonName);
+  var dest          = resolvePath(addonName);
   // check for the mode, is running test or not
   var isRunningTest = options.test || false;
 
@@ -59,23 +72,19 @@ module.exports = function runTasks (addonName, options) {
     // Running initialization tasks: npm install,
     .then(function () {
       if (!isRunningTest) {
-        installerTasks(addonName, dest);
+        installerTasks(dest);
       }
     })
     .catch(function () {
-      log(red('[-Error:]'), 'ng-addon could not generate an addon package due to the error above!');
+      errorLogger('ng-addon could not generate an addon package due to the error above!');
       process.exit(0);
     });
 }
 
-// When `ngg new` command succeed and output successfully message to user
-function successInfoLogger (addonName) {
-  log(green('[-done:] Initialized a new git repo and did a first commit'));
-  log(bold('[-copy:] =>'),
-    cyan('cd ' + addonName), gray('# navigate to the newly created addon package'));
-  log(bold('[-copy:] =>'), cyan('ngg serve'), gray('watch file changes and rebuild the addon'));
+function successLogger (msg) {
+  log(green('[-done:] ' + msg));
 }
 
-function failureInfoLogger (err) {
-  log(red('[-Error:] ' + err));
+function errorLogger (msg) {
+  log(red('[-Error:] ' + msg));
 }
